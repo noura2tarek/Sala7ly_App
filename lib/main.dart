9033448a6e66122fcbf756a4a7108a5e6fa7f4a1
Noura Tarek <99992263+noura2tarek@users.noreply.github.com>
2024-05-27 20:30:04 +0300

    edit in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sala7ly_app/core/resources/routes_manager.dart';
-import 'package:sala7ly_app/view/screens/main_page_layout/carpentry_issue_page.dart';
-import 'package:sala7ly_app/view/screens/main_page_layout/carpentry_page.dart';
-import 'package:sala7ly_app/view/screens/main_page_layout/electrical_appliances_page.dart';
-import 'package:sala7ly_app/view/screens/main_page_layout/electricity_page.dart';
-import 'package:sala7ly_app/view/screens/main_page_layout/painting_page.dart';
+import 'package:sala7ly_app/view/screens/start_page/start_page.dart';
 import 'core/resources/app-strings.dart';
 
 void main() {
@@ -23,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.brown),
         useMaterial3: true,
       ),
-      home: CarpentryScreen(),
+      home: StartPage(),
       routes: routes,
     );
   }
